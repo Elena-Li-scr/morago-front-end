0401618aa60fd319a7159b1be0b9968c81978680
valdimir <you@example.com>
2025-09-19 17:13:19 +0900

    fix bugs ui

--- apps/translator/src/components/headers_footers/BalanceHeader.tsx
+++ apps/translator/src/components/headers_footers/BalanceHeader.tsx
@@ -18,6 +18,10 @@ export default function BalanceHeader() {
   const isHomePage = location.pathname === "/my-home-translator-page";
   const isBalancePage = location.pathname === "/my-balance-translator-page";
   const isCallHistory = location.pathname === "/my-call-history";
+  const isMyPtofile = location.pathname.includes("/my-profile-page");
+  const isNotification = location.pathname.includes("/my-notification-page");
+
+  if (isMyPtofile || isNotification) return;
 
   return (
     <div
