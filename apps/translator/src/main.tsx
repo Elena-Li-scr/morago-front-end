ca606a294df68914374848e2a3e5497b867c30c5
KimVladimir17 <vladimir.kim200017@gmail.com>
2025-09-19 12:13:52 +0900

    Update main.tsx

--- apps/translator/src/main.tsx
+++ apps/translator/src/main.tsx
@@ -10,15 +10,4 @@ createRoot(document.getElementById("root")!).render(
   </React.StrictMode>,
 );
 
-// // import { StrictMode } from "react";
-// import { createRoot } from "react-dom/client";
-// import "../../../shared/styles/index.css";
-// import App from "./App.tsx";
-// import { CallProvider } from "@shared/components/webRtc/CallProvider";
 
-// const token = localStorage.getItem("token") || "";
-// createRoot(document.getElementById("root")!).render(
-//   <CallProvider role="TRANSLATOR" wsUrl="http://localhost:8080/ws" token={token}>
-//     <App />
-//   </CallProvider>,
-// );
