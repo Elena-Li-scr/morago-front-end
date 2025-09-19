4e5c9c37ce2830c35ca8860179777884bd52baf2
valdimir <you@example.com>
2025-09-19 12:23:36 +0900

    cleare code

--- apps/translator/src/main.tsx
+++ apps/translator/src/main.tsx
@@ -9,16 +9,3 @@ createRoot(document.getElementById("root")!).render(
     <App />
   </React.StrictMode>,
 );
-
-// // import { StrictMode } from "react";
-// import { createRoot } from "react-dom/client";
-// import "../../../shared/styles/index.css";
-// import App from "./App.tsx";
-// import { CallProvider } from "@shared/components/webRtc/CallProvider";
-
-// const token = localStorage.getItem("token") || "";
-// createRoot(document.getElementById("root")!).render(
-//   <CallProvider role="TRANSLATOR" wsUrl="http://localhost:8080/ws" token={token}>
-//     <App />
-//   </CallProvider>,
-// );
